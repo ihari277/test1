90a3cf705e3ae7370126382b0ea274ccdb39fdc0
ihari277 <50480202+ihari277@users.noreply.github.com>
2020-02-18 19:08:42 +0530

    Update new.py

--- new.py
+++ new.py
@@ -1,8 +1,29 @@
-import sys
-sys.exit(-1)
-a = int(input("enter first number: "))
-b = int(input("enter second number: "))
- 
-sum = a + b
- 
-print("sum:", sum)
+import java.util.Scanner;
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+        /* This reads the input provided by user
+         * using keyboard
+         */
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+
+        // This method reads the number provided using keyboard
+        int num1 = scan.nextInt();
+        
+        System.out.print("Enter second number: ");
+        int num2 = scan.nextInt();
+
+        // Closing Scanner after the use
+        scan.close();
+        
+        // Calculating product of two numbers
+        int product = num1*num2;
+        
+        // Displaying the multiplication result
+        System.out.println("Output: "+product);
+    }
+}
+Output:
